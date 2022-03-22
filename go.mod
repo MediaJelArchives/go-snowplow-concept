8444b4fd8026fded0dfaef9d437f954e58bce9b0
pacholoamit <pacholoamit.tech@gmail.com>
2022-03-22 22:38:06 +0800

    removed events.db

--- go.mod
+++ go.mod
@@ -3,8 +3,12 @@ module mediajel/go-snowplow-concept
 go 1.18
 
 require (
+	github.com/gin-gonic/gin v1.7.7
+	github.com/snowplow/snowplow-golang-tracker/v2 v2.4.1
+)
+
+require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/gin-gonic/gin v1.7.7 // indirect
 	github.com/go-playground/locales v0.13.0 // indirect
 	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/go-playground/validator/v10 v10.4.1 // indirect
@@ -19,7 +23,6 @@ require (
 	github.com/mattn/go-sqlite3 v2.0.2+incompatible // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
-	github.com/snowplow/snowplow-golang-tracker/v2 v2.4.1 // indirect
 	github.com/ugorji/go/codec v1.1.7 // indirect
 	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
 	golang.org/x/sys v0.0.0-20200116001909-b77594299b42 // indirect
--- main.go
+++ main.go
@@ -5,9 +5,25 @@ import (
 	sp "github.com/snowplow/snowplow-golang-tracker/v2/tracker"
 )
 
+func main() {
+	router := gin.Default()
+	router.Use(TrackerMiddleWare())
+	router.GET("/pageview", PageviewTrackingDemo)
+	router.Run("localhost:3000")
+}
+
 func TrackerMiddleWare() gin.HandlerFunc {
-	// Instantiate a subject
+	// Instantiate a subject & override values
 	subject := sp.InitSubject()
+	subject.SetUserId("{{USER_ID}}")
+	subject.SetDomainUserId("{{DOMAIN_USER_ID")
+	subject.SetIpAddress("{{IP_ADDRESS}}")
+	subject.SetNetworkUserId("{{NETWORK_USER_ID}}")
+	subject.SetUseragent("{{USER_AGENT}}")
+	subject.SetTimeZone("{{TIME_ZONE}}")
+	subject.SetViewPort(420, 420)
+	subject.SetScreenResolution(420, 420)
+
 	// Initialize tracker config
 	emitter := sp.InitEmitter(sp.RequireCollectorUri("collector.dmp.mediajel.ninja"))
 	tracker := sp.InitTracker(sp.RequireEmitter(emitter),
@@ -16,29 +32,54 @@ func TrackerMiddleWare() gin.HandlerFunc {
 		sp.OptionAppId("golang-snowplow"),
 		sp.OptionPlatform("srv"),
 		sp.OptionBase64Encode(false))
-	
-		return func(context *gin.Context) {
-			context.Set("tracker", tracker)
-			context.Set("subject", subject)
-			context.Next()
-		}
+
+	return func(context *gin.Context) {
+		context.Set("tracker", tracker)
+		context.Set("subject", subject)
+		context.Next()
+	}
 }
 
-
-
-func PageviewTrackingDemo (context *gin.Context) {
+func PageviewTrackingDemo(context *gin.Context) {
 	tracker := context.MustGet("tracker").(*sp.Tracker)
 	subject := context.MustGet("subject").(*sp.Subject)
 
-	 tracker.TrackPageView(sp.PageViewEvent{
-	 	PageUrl: sp.NewString("localhost:3000"),
-	 	Subject: subject,
-	 })
+	tracker.TrackPageView(sp.PageViewEvent{
+		PageUrl: sp.NewString("{{DOMAIN_URL}}"),
+		Subject: subject,
+	})
+
 }
 
-func main() {
-	router := gin.Default()
-	router.Use(TrackerMiddleWare())
-	router.GET("/pageview", PageviewTrackingDemo)
-	router.Run("localhost:3000")
+func EcommerceTrackingDemo(context *gin.Context) {
+	tracker := context.MustGet("tracker").(*sp.Tracker)
+	// subject := context.MustGet("subject").(*sp.Subject)
+
+	items := []sp.EcommerceTransactionItemEvent{
+		sp.EcommerceTransactionItemEvent{
+			Sku:      sp.NewString("pbz0026"),
+			Price:    sp.NewFloat64(20),
+			Quantity: sp.NewInt64(1),
+		},
+		sp.EcommerceTransactionItemEvent{
+			Sku:      sp.NewString("pbz0038"),
+			Price:    sp.NewFloat64(15),
+			Quantity: sp.NewInt64(1),
+			Name:     sp.NewString("red hat"),
+			Category: sp.NewString("menswear"),
+		},
+	}
+	tracker.TrackEcommerceTransaction(sp.EcommerceTransactionEvent{
+		OrderId:     sp.NewString("6a8078be"),
+		TotalValue:  sp.NewFloat64(35),
+		Affiliation: sp.NewString("some-affiliation"),
+		TaxValue:    sp.NewFloat64(6.12),
+		Shipping:    sp.NewFloat64(30),
+		City:        sp.NewString("Dijon"),
+		State:       sp.NewString("Bourgogne"),
+		Country:     sp.NewString("France"),
+		Currency:    sp.NewString("EUR"),
+		Items:       items,
+	})
+
 }
